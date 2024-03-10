00ba26fafda4112f992b129ffb75b97262120879
pcshen0828 <michelleshen0828@gmail.com>
2024-03-10 22:37:45 +0800

    Init app.

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-playground',
+
+  projectId: 'eg9zdexo',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
